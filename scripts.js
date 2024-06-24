fee566ce5f55a3d4dccdc2367e6ae171c268887d
Anubhav Mishra <mishra.anubhav108@gmail.com>
2024-06-24 11:59:10 +0530

    completed the UI feautre

--- scripts.js
+++ scripts.js
@@ -1,44 +1,59 @@
-function humanInput(){
-    let hInput=prompt("Choose Rock, Paper, Scissors: ");
-    return hInput;
-}
+const buttonContainer = document.querySelector(".button-container");
 
-function randIntGenerator(){
-    let randInt=Math.floor(Math.random()*10);
-    return randInt;
-}
-
-function computerInput(){
-    let cInput = "";
-    let randInt = randIntGenerator();
-    if (randInt==0) return computerInput();
-    else if (randInt==1 || randInt==2 || randInt==3) {cInput="Rock"; return cInput;}
-    else if (randInt==4 || randInt==5 || randInt==6) {cInput="Paper"; return cInput;}
-    else if (randInt==7 || randInt==8 || randInt==9) {cInput="Scissors"; return cInput;}
-}
-
-function winnerCheck(){
-    let hInput = humanInput();
-    let cInput = computerInput();
-    if (hInput=="Rock"){
-        if (cInput=="Paper") return "Computer wins";
-        else if (cInput=="Scissors") return "Human wins";
-        else if (cInput=="Rock") return "Draw";
+buttonContainer.addEventListener("click", (e) => {
+    if (e.target.tagName !== "BUTTON") return; // Ensure a button was clicked
+    function humanInput(){
+        return e.target.textContent;
     }
 
-    else if (hInput=="Paper"){
-        if (cInput=="Paper") return "Draw";
-        else if (cInput=="Scissors") return "Computer wins";
-        else if (cInput=="Rock") return "Human wins";
+    function randIntGenerator(){
+        return Math.floor(Math.random()*10);
     }
 
-    else if (hInput=="Scissors"){
-        if (cInput=="Paper") return "Human wins";
-        else if (cInput=="Scissors") return "Draw";
-        else if (cInput=="Rock") return "Computer wins";
+    function computerInput(){
+        let randInt=randIntGenerator();
+        if (randInt==0) return computerInput();
+        else if (randInt==1 || randInt == 2 || randInt == 3) return "Rock";
+        else if (randInt==4 || randInt == 5 || randInt == 6) return "Paper";
+        else if (randInt==7 || randInt == 8 || randInt == 9) return "Scissors"; 
     }
-}
 
-//main
+    function winnerCheck(){
+        let hInput = humanInput();
+        let cInput = computerInput();
+        if (hInput=="Rock"){
+            if (cInput=="Paper") return "Computer wins";
+            else if (cInput=="Scissors") return "Human wins";
+            else if (cInput=="Rock") return "Draw";
+        }
+    
+        else if (hInput=="Paper"){
+            if (cInput=="Paper") return "Draw";
+            else if (cInput=="Scissors") return "Computer wins";
+            else if (cInput=="Rock") return "Human wins";
+        }
+    
+        else if (hInput=="Scissors"){
+            if (cInput=="Paper") return "Human wins";
+            else if (cInput=="Scissors") return "Draw";
+            else if (cInput=="Rock") return "Computer wins";
+        }
+    }
+    
+    const result = document.querySelector("p");
+    result.textContent = "";
 
-console.log(winnerCheck());
+    result.textContent = winnerCheck();
+
+
+    let realHScore = document.querySelector(".human span");
+    let realCScore = document.querySelector(".computer span");
+
+    let tempHScore=Number(realHScore.textContent);
+    let tempCScore=Number(realCScore.textContent);
+    if (result.textContent === "Human wins") tempHScore++;
+    else if (result.textContent === "Computer wins") tempCScore++;
+
+    realHScore.textContent=tempHScore;
+    realCScore.textContent=tempCScore;
+})
